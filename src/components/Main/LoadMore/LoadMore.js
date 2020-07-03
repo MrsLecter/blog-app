070dbc93dbce15df73d0deffc0879ee0730c3977
Maksym Baboshko <maksym.baboshko@yahoo.com>
2020-07-03 12:57:43 +0300

    Bug fixed

--- src/components/Main/LoadMore/LoadMore.js
+++ src/components/Main/LoadMore/LoadMore.js
@@ -4,7 +4,8 @@ export default function LoadMore() {
   return (
     <div className="uk-margin">
       <button className="uk-button uk-button-primary uk-width-1-1 uk-margin-small-bottom">
-        Load more <div className="uk-margin-small-left" uk-spinner="ratio: 0.6" />
+        Load more
+        {/* <div className="uk-margin-small-left" uk-spinner="ratio: 0.6" /> //Анимация загрузки */}
       </button>
     </div>
   )
