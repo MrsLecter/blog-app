954133e31485511955a7a6f16ec45c8df8f07765
Maksym Baboshko <maksym.baboshko@yahoo.com>
2020-07-03 13:59:06 +0300

    Updates

---
+++ precache-manifest.782dbdeff68d3a6b5d4b9b1c473e33a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2dc7d3f55f895a4b187bae87dcc44c",
+    "url": "/blog-app/index.html"
+  },
+  {
+    "revision": "24d2ad3e97d2a282da94",
+    "url": "/blog-app/static/js/2.54919364.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blog-app/static/js/2.54919364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c5c8ff337b0ddb25e2e",
+    "url": "/blog-app/static/js/main.509ce87a.chunk.js"
+  },
+  {
+    "revision": "bc8aebfaceecca805670",
+    "url": "/blog-app/static/js/runtime-main.25cf73be.js"
+  }
+]);
